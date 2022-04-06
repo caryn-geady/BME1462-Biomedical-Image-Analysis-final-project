bfe4a6ed007f8e1641fd63e39222ec468c8ee258
AndrewE9 <effatand@gmail.com>
2022-04-06 12:36:57 -0400

    Added normality check, non-parametric testing, outlier filter

--- scripts/RegionStats.py
+++ scripts/RegionStats.py
@@ -9,6 +9,12 @@ from statannot import add_stat_annotation
 
 df = pd.read_csv('data.csv')
 
+def reject_outliers_2(data, m=2.):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d / (mdev if mdev else 1.)
+    return data[s < m]
+
 def RegionStats(DataFrame):
 
     metrics = df['variable'].unique()
@@ -16,7 +22,12 @@ def RegionStats(DataFrame):
     i = 0
     Met = []
     Reg = []
-    Pv = []
+    Resp_count = []
+    NonResp_count = []
+    Resp_Norm_pv = []
+    NonResp_Norm_pv = []
+    Wtest_pv = []
+    Mw_pv = []
     for metric in metrics:
         # visualizing the intensity distributions
         fig, ax = plt.subplots()
@@ -45,16 +56,32 @@ def RegionStats(DataFrame):
         # t-test between the rim and perivascular intensity calculations
         responders = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] == True)]['value'].values
         non_responders = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] == False)]['value'].values
+
         responders = responders[~np.isnan(responders)]
         non_responders = non_responders[~np.isnan(non_responders)]
-        ttest = stats.ttest_ind(responders, non_responders)
+        responders = reject_outliers_2(responders)
+        non_responders = reject_outliers_2(non_responders)
+
+        k, p = stats.normaltest(responders)
+        k2, p2 = stats.normaltest(non_responders)
+
+        manwit = stats.mannwhitneyu(responders,non_responders)
+        ttest = stats.ttest_ind(responders, non_responders, equal_var=False)
+
         # keep here to inspect result
         Met.append(metric)
         Reg.append(metric.split(' ')[0])
-        Pv.append(ttest.pvalue)
+        Resp_count.append(len(responders))
+        NonResp_count.append(len(non_responders))
+        Resp_Norm_pv.append(p)
+        NonResp_Norm_pv.append(p2)
+        Wtest_pv.append(ttest.pvalue)
+        Mw_pv.append(manwit.pvalue)
         i+=1
-    results = pd.DataFrame(list(zip(Met,Reg,Pv)),
-               columns =['Metrics', 'Region','P-value'])
+    results = pd.DataFrame(list(zip(Met,Reg,Resp_count, NonResp_count, Resp_Norm_pv, NonResp_Norm_pv, Wtest_pv, Mw_pv)),
+               columns =['Metrics', 'Region','Responder Sample Size','Non-Responder Sample Size','Responder Normality P-value', 'Non-Responder Normality P-value', 'Welch test P-value', 'Mann Whitney P-value'])
     return results
 results = RegionStats(df)
 results.to_csv('Results.csv')
+
+        
