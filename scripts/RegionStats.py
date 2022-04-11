2139c4c1c088754b2b43ec34ae8a2f502d1116ec
AndrewE9 <effatand@gmail.com>
2022-04-11 13:32:07 -0400

    testing

--- scripts/RegionStats.py
+++ scripts/RegionStats.py
@@ -6,14 +6,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 from statannot import add_stat_annotation
-import matplotlib.pyplot as plt
-
-from statannotations.Annotator import Annotator
-
-
-
 
 df = pd.read_csv('data.csv')
+
+def reject_outliers_2(data, m=2.):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d / (mdev if mdev else 1.)
+    return data[s < m]
 
 def RegionStats(DataFrame):
 
@@ -22,96 +22,65 @@ def RegionStats(DataFrame):
     i = 0
     Met = []
     Reg = []
-    PosResp_count = []
+    Resp_count = []
     NonResp_count = []
-    NegResp_count = []
-    PosResp_Norm_pv = []
+    Resp_Norm_pv = []
     NonResp_Norm_pv = []
-    NegResp_Norm_pv = []
-    Mw1_pv = []
-    Mw2_pv = []
-    Mw3_pv = []
-
+    Wtest_pv = []
+    Mw_pv = []
     for metric in metrics:
         # visualizing the intensity distributions
-        plt.style.use('dark_background')
         fig, ax = plt.subplots()
-        ax = sns.boxplot(data=df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] != 9)], x = 'Response', y = 'value', order = ['r','n','g'])
-        x = "Response"
-        y = "value"
-        order = ['r','n','g']
-        pairs=[('r','n'),('r','g'),('n','g')]
-        test = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] != 9)]
-        annotator = Annotator(ax, pairs, data=test, x=x, y=y, order=order)
-        annotator.configure(test='Mann-Whitney', text_format='star', loc='inside')
-        annotator.apply_and_annotate()
-
+        ax = sns.boxplot(data=df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric)], x = 'Response', y = 'value')
+        
         if 'VOL' in metric:
-            ax.set_title('Baseline tumour volume', fontsize = fs+2)
-            ax.set_xlabel('', fontsize = fs)
-            ax.set_xticklabels(labels = ['Volume Reduced', 'No Response','Volume Increased'], fontsize = fs)
-            ax.set_ylabel('Volume ($cm^3$)', fontsize = fs)
+            ax.set_title('Average Tumour Volume')
+            #ax.set_xlabel('Response')
+            ax.set_xticklabels(labels = ['No Response','Response'])
+            ax.set_ylabel('Volume ($mm^3$)')
             plt.savefig('Figure{0}.png'.format(i),bbox_inches = 'tight')
         elif 'mu' in metric:
             x = metric.split(' ')[0]
-            ax.set_title('CT intensity in {0}'.format(x), fontsize = fs+2)
-            ax.set_xlabel('', fontsize = fs)
-            ax.set_xticklabels(labels = ['Volume Reduced', 'No Response','Volume Increased'], fontsize = fs)
-            ax.set_ylabel('CT intensity (HU)', fontsize = fs)
-            ax.set_ylim(-900,400)
+            ax.set_title('Average Hounsfield Units in {0}'.format(x))
+            #ax.set_xlabel('Response')
+            ax.set_xticklabels(labels = ['No Response','Response'])
+            ax.set_ylabel('Hounsfield Units')
             plt.savefig('Figure{0}.png'.format(i),bbox_inches = 'tight')
         elif 'S' in metric:
             x = metric.split(' ')[0]
-            ax.set_title('Entropy in {0}'.format(x), fontsize = fs+2)
-            ax.set_xlabel('', fontsize = fs)
-            ax.set_xticklabels(labels = ['Volume Reduced', 'No Response','Volume Increased'], fontsize = fs)
-            ax.set_ylabel('Entropy (1)', fontsize = fs)
-            ax.set_ylim(1,3.0)
+            ax.set_title('Average Entropy in {0}'.format(x))
+            #ax.set_xlabel('Response')
+            ax.set_xticklabels(labels = ['No Response','Response'])
+            ax.set_ylabel('Entropy Units?')
             plt.savefig('Figure{0}.png'.format(i),bbox_inches = 'tight')
-
-
-
         # t-test between the rim and perivascular intensity calculations
-        positive_responders = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] == 'r')]['value'].values
-        non_responders = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] == 'n')]['value'].values
-        negative_responders = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] == 'g')]['value'].values
+        responders = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] == True)]['value'].values
+        non_responders = df.loc[(df['TIMEPT'] == 'BL') & (df['variable'] == metric) & (df['Response'] == False)]['value'].values
 
-        positive_responders = positive_responders[~np.isnan(positive_responders)]
+        responders = responders[~np.isnan(responders)]
         non_responders = non_responders[~np.isnan(non_responders)]
-        negative_responders = negative_responders[~np.isnan(negative_responders)]
+        responders = reject_outliers_2(responders)
+        non_responders = reject_outliers_2(non_responders)
 
+        k, p = stats.normaltest(responders)
+        k2, p2 = stats.normaltest(non_responders)
 
-        #ttest = stats.ttest_ind(positive_responders, negative_responders, equal_var=False)
-        manwit1 = stats.mannwhitneyu(positive_responders,non_responders)
-        manwit2 = stats.mannwhitneyu(positive_responders,negative_responders)
-        manwit3 = stats.mannwhitneyu(non_responders,negative_responders)
-        #kruk = stats.kruskal(positive_responders, non_responders, negative_responders)
+        manwit = stats.mannwhitneyu(responders,non_responders)
+        ttest = stats.ttest_ind(responders, non_responders, equal_var=False)
 
         # keep here to inspect result
         Met.append(metric)
         Reg.append(metric.split(' ')[0])
-
-        PosResp_count.append(len(positive_responders))
+        Resp_count.append(len(responders))
         NonResp_count.append(len(non_responders))
-        NegResp_count.append(len(negative_responders))
-
-        PosResp_Norm_pv.append(stats.shapiro(positive_responders).pvalue)
-        NonResp_Norm_pv.append(stats.shapiro(non_responders).pvalue)
-        NegResp_Norm_pv.append(stats.shapiro(negative_responders).pvalue)
-
-
-        Mw1_pv.append(manwit1.pvalue)
-        Mw2_pv.append(manwit2.pvalue)
-        Mw3_pv.append(manwit3.pvalue)
+        Resp_Norm_pv.append(p)
+        NonResp_Norm_pv.append(p2)
+        Wtest_pv.append(ttest.pvalue)
+        Mw_pv.append(manwit.pvalue)
         i+=1
-    results = pd.DataFrame(list(zip(Met,Reg,PosResp_count,NonResp_count,NegResp_count,PosResp_Norm_pv,NonResp_Norm_pv,NegResp_Norm_pv,Mw1_pv,Mw2_pv,Mw3_pv)),
-               columns =['Metrics', 'Region','Positive Responder Sample Size','Non-Responder Sample Size','Negative Responder Sample Size',
-               'Responder Normality P-value', 'Non-Responder Normality P-value','Negative Responder Normality P-value', 'Reduction/No Response P-value', 'Reduction/Increase P-value', 'No Response/Increase P-value'])
+    results = pd.DataFrame(list(zip(Met,Reg,Resp_count, NonResp_count, Resp_Norm_pv, NonResp_Norm_pv, Wtest_pv, Mw_pv)),
+               columns =['Metrics', 'Region','Responder Sample Size','Non-Responder Sample Size','Responder Normality P-value', 'Non-Responder Normality P-value', 'Welch test P-value', 'Mann Whitney P-value'])
     return results
-
-
-fs = 12
-
 results = RegionStats(df)
 results.to_csv('Results.csv')
 
--- scripts/ResponseAssignment.py
+++ scripts/ResponseAssignment.py
@@ -1,150 +1,40 @@
-from cmath import isnan
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-def remove_outliers2(data, threshold=3.5):
-        """
-        Median Absolute Deviation (MAD) based outlier detection
-        Removes outliers and if selected fills with polynomial  interpolation
-        fill: Boolean
-        https://www.programcreek.com/python/?CodeExample=remove+outliers
-        https://www.itl.nist.gov/div898/handbook/eda/section3/eda356.htm#MAD
-        https://www.itl.nist.gov/div898/handbook/eda/section3/eda35h.htm
-        """
-        median = np.median(data)
 
-        med_abs_deviation = np.median(abs(data - median))
-        # scale constant 0.6745
-        modified_z_score = 0.6745 * (data - median)/ med_abs_deviation
-        data[modified_z_score > threshold] = np.nan
-
-        return data 
-def RemoveMissingPairs(DataFrame):
-    ''' (dataframe) --> dataframe
-    Add response flag based on pre and post tumour volumes
-    '''
-    # loop through each unique patient
-    for pt in DataFrame['USUBJID'].unique():
-        criterion_1 = (DataFrame['USUBJID'] == pt)
-        # loop through each lesion for each patient
-        for lesion_index in DataFrame.loc[criterion_1]['LSNIND'].unique():
-            criterion_2 = (DataFrame['LSNIND'] == lesion_index)
-            criterion_3a = (DataFrame['TIMEPT'] == 'BL')
-            criterion_3b = (DataFrame['TIMEPT'] == 'C2')
-
-            # find the pre/post volumes and indices
-            vol_pre = DataFrame.loc[criterion_1 & criterion_2 & criterion_3a]['LSNVOL'].values
-            vol_post = DataFrame.loc[criterion_1 & criterion_2 & criterion_3b]['LSNVOL'].values
-            vol_pre_index = DataFrame.index[criterion_1 & criterion_2 & criterion_3a]
-            vol_post_index = DataFrame.index[criterion_1 & criterion_2 & criterion_3b]
-            
-            if np.size(vol_pre) == 0 or np.size(vol_post) == 0:
-                DataFrame.loc[vol_pre_index, 'Response'] = np.nan
-                DataFrame.loc[vol_post_index, 'Response'] = np.nan
-            else:
-                DataFrame.loc[vol_pre_index, 'Response'] = 9
-                DataFrame.loc[vol_post_index, 'Response'] = 9
+# define dataframe with patient/lesion data
+df = pd.read_csv('results_5Apr2022.csv')
 
 def ResponseAssignment(DataFrame):
     ''' (dataframe) --> dataframe
     Add response flag based on pre and post tumour volumes
     '''
     # loop through each unique patient
-    for pt in DataFrame['USUBJID'].unique():
-        criterion_1 = (DataFrame['USUBJID'] == pt)
+    for pt in df['USUBJID'].unique():
+        criterion_1 = (df['USUBJID'] == pt)
         # loop through each lesion for each patient
-        for lesion_index in DataFrame.loc[criterion_1]['LSNIND'].unique():
-            criterion_2 = (DataFrame['LSNIND'] == lesion_index)
-            criterion_3a = (DataFrame['TIMEPT'] == 'BL')
-            criterion_3b = (DataFrame['TIMEPT'] == 'C2')
+        for lesion_index in df.loc[criterion_1]['LSNIND'].unique():
+            criterion_2 = (df['LSNIND'] == lesion_index)
+            criterion_3a = (df['TIMEPT'] == 'BL')
+            criterion_3b = (df['TIMEPT'] == 'C2')
 
             # find the pre/post volumes and indices
-            vol_pre = DataFrame.loc[criterion_1 & criterion_2 & criterion_3a]['LSNVOL'].values
-            vol_post = DataFrame.loc[criterion_1 & criterion_2 & criterion_3b]['LSNVOL'].values
-            vol_pre_index = DataFrame.index[criterion_1 & criterion_2 & criterion_3a]
-            vol_post_index = DataFrame.index[criterion_1 & criterion_2 & criterion_3b]
+            vol_pre = df.loc[criterion_1 & criterion_2 & criterion_3a]['LSNVOL'].values
+            vol_post = df.loc[criterion_1 & criterion_2 & criterion_3b]['LSNVOL'].values
+            vol_pre_index = df.index[criterion_1 & criterion_2 & criterion_3a]
+            vol_post_index = df.index[criterion_1 & criterion_2 & criterion_3b]
             
-            if np.size(vol_pre) == 0 or np.size(vol_post) == 0:
-                DataFrame.loc[vol_pre_index, 'Response'] = np.nan
-                DataFrame.loc[vol_post_index, 'Response'] = np.nan
-                continue
             # ratio of post vs pre volume
             ratio = vol_post/vol_pre
-                
+            
             # create response flag in the dataframe
-            if ratio <=0.1:
-                DataFrame.loc[vol_pre_index, 'Response'] = 'r'
-                DataFrame.loc[vol_post_index, 'Response'] = 'r'
-            elif ratio <= 0.85:
-                DataFrame.loc[vol_pre_index, 'Response'] = 'r'
-                DataFrame.loc[vol_post_index, 'Response'] = 'r'
-            elif 0.85 < ratio < 1.15:
-                DataFrame.loc[vol_pre_index, 'Response'] = 'n'
-                DataFrame.loc[vol_post_index, 'Response'] = 'n'
-            elif 1.15 <= ratio <= 1.9:
-                DataFrame.loc[vol_pre_index, 'Response'] = 'g'
-                DataFrame.loc[vol_post_index, 'Response'] = 'g'
+            if ratio <= 0.8:
+                df.loc[vol_pre_index, 'Response'] = True
+                df.loc[vol_post_index, 'Response'] = True
             else:
-                DataFrame.loc[vol_pre_index, 'Response'] = 'g'
-                DataFrame.loc[vol_post_index, 'Response'] = 'g'
+                df.loc[vol_pre_index, 'Response'] = False
+                df.loc[vol_post_index, 'Response'] = False
 
-# define dataframe with patient/lesion data
-df = pd.read_csv('results_5Apr2022.csv')
-
-df = df.loc[(df['ARM'] ==True)]
-print(df.shape[0])
-df['LSNVOL'] = df['LSNVOL'].div(1000)
-df['LSNVOL'] = df['LSNVOL'].replace(0,np.nan)
-
-
-# drop nan values
-df = df.dropna()
-df = df.reset_index(drop=True)
-
-RemoveMissingPairs(df)
-
-df = df.dropna()
-df = df.reset_index(drop=True)
-
-df2 = df.loc[df['TIMEPT'] == 'BL']
-df3 = df.loc[df['TIMEPT'] == 'C2']
-df2['LSNVOL'] = remove_outliers2(df2['LSNVOL'])
-df2 = df2.dropna()
-df2 = df2.reset_index(drop=True)
-
-df3['LSNVOL'] = remove_outliers2(df3['LSNVOL'])
-df3 = df3.dropna()
-df3 = df3.reset_index(drop=True)
-
-df = pd.concat([df2,df3])
 ResponseAssignment(df)
-
-df = df.dropna()
-df = df.reset_index(drop=True)
-
-
-#df = df.loc[df['TIMEPT'] == 'BL']
-
-fs = 14
-plt.style.use('dark_background')
-#df['LSNVOL'].hist(bins=range(0,500,50), color = 'blue')
-df['LSNVOL'].hist(bins=10, color = 'blue')
-plt.xlabel('Volume ($cm^3$)', fontsize = fs)
-plt.ylabel('Frequency', fontsize = fs)
-#plt.xlim(0,400)
-plt.xlim(0,10)
-plt.xticks(fontsize = fs)
-#plt.ylim(0,140)
-plt.ylim(0,20)
-plt.yticks(fontsize = fs)
-
-#plt.title('Lesion volume histogram: prior to filtering', fontsize = fs+2)
-plt.title('Lesion volume histogram: post filtering', fontsize = fs+2)
-plt.show()
-
-print(df.shape[0])
-# reshape dataframe to be more friendly for stats functions
 headers = ['USUBJID','TIMEPT','ARM','LSNIND','Response']
 df = pd.melt(df,id_vars=headers)
 df.to_csv('data.csv')
